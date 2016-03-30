90f8b065fc5c5e02b957b09fc7a71bce536899f2
cph-al <dressing2cool@hotmail.com>
2016-03-30 13:13:09 +0200

    Update PolygonDB.sql

--- PolygonDB.sql
+++ PolygonDB.sql
@@ -7,16 +7,17 @@ DROP TABLE IF EXISTS customers;
 
 CREATE TABLE buildings	(
 	bName VARCHAR(20) NOT NULL,
-    Address VARCHAR(25) NOT NULL,
-    bParcel INT NOT NULL,
-    bFloors INT NOT NULL,
-    bSize INT NOT NULL,
-    bStatus INT NOT NULL,
+    	Address VARCHAR(25) NOT NULL,
+    	bParcel INT NOT NULL,
+    	bFloors INT NOT NULL,
+    	bSize INT NOT NULL,
+    	bStatus INT NOT NULL,
 Primary KEY (bParcel));
 
 CREATE TABLE CUSTOMERS (
 	cID INT NOT NULL,
-	cName VARCHAR(20) NOT NULL, 
-    bParcel INT NOT NULL,
-    primary key (cID),
-    foreign key (bParcel) references buildings(bParcel));
+	cName VARCHAR(20) NOT NULL,
+    	cAddress VARCHAR(25) NOT NULL,
+	 bParcel INT NOT NULL,
+primary key (cID),
+foreign key (bParcel) references buildings(bParcel));
