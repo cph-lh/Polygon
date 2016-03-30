ba0464442fb2b145c500c19072187e6f45aafe52
laurahartig <laurahartig@10.20.154.109>
2016-03-30 14:06:04 +0200

    DB edits (address)

--- PolygonDB.sql
+++ PolygonDB.sql
@@ -7,7 +7,7 @@ DROP TABLE IF EXISTS customers;
 
 CREATE TABLE buildings	(
 	bName VARCHAR(20) NOT NULL,
-    	Address VARCHAR(25) NOT NULL,
+    	bAddress VARCHAR(25) NOT NULL,
     	bParcel INT NOT NULL,
     	bFloors INT NOT NULL,
     	bSize INT NOT NULL,
@@ -15,14 +15,13 @@ CREATE TABLE buildings	(
 Primary KEY (bParcel));
 
 CREATE TABLE customers (
-		cID INT NOT NULL,
-		cName VARCHAR(20) NOT NULL,
-    	cAddress VARCHAR(25) NOT NULL,
+        cID INT NOT NULL,
+	cName VARCHAR(20) NOT NULL,
         cPhone INT(8),
-		bParcel INT NOT NULL,
-        cPass VARCHAR (15),
+	bParcel INT NOT NULL,
+        cPassword VARCHAR (15),
 primary key (cID),
 foreign key (bParcel) references buildings(bParcel));
 
-insert into buildings values ('
-insert into customers VALUES ('1111','TestPerson1','Nowherestreet 323','22334455','98756','1234');
+insert into buildings values ();
+insert into customers VALUES ('1111','TestPerson1','22334455','98756','1234');
