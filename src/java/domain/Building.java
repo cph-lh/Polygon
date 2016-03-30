c81204f84481a7f8bf6b8d7f920bdba4d2fb426c
laurahartig <laurahartig@10.20.154.109>
2016-03-30 12:15:56 +0200

    Building object class

---
+++ src/java/domain/Building.java
@@ -0,0 +1,82 @@
+package domain;
+
+public class Building
+{
+    String name;
+    String address;
+    int parcel;
+    int floors;
+    int size;
+    int status;
+
+    public Building(String name, String address, int parcel, int floors, int size, int status)
+    {
+        this.name = name;
+        this.address = address;
+        this.parcel = parcel;
+        this.floors = floors;
+        this.size = size;
+        this.status = status;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getAddress()
+    {
+        return address;
+    }
+
+    public void setAddress(String address)
+    {
+        this.address = address;
+    }
+
+    public int getParcel()
+    {
+        return parcel;
+    }
+
+    public void setParcel(int parcel)
+    {
+        this.parcel = parcel;
+    }
+
+    public int getFloors()
+    {
+        return floors;
+    }
+
+    public void setFloors(int floors)
+    {
+        this.floors = floors;
+    }
+
+    public int getSize()
+    {
+        return size;
+    }
+
+    public void setSize(int size)
+    {
+        this.size = size;
+    }
+
+    public int getStatus()
+    {
+        return status;
+    }
+
+    public void setStatus(int status)
+    {
+        this.status = status;
+    }
+    
+}
