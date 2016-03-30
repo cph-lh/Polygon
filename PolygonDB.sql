1997f7e7cdd1d6f103cf4f68b46526f14d2166a3
Andreas <Andreas@10.20.156.109>
2016-03-30 13:58:21 +0200

    DB changes

--- PolygonDB.sql
+++ PolygonDB.sql
@@ -15,9 +15,14 @@ CREATE TABLE buildings	(
 Primary KEY (bParcel));
 
 CREATE TABLE customers (
-	cID INT NOT NULL,
-	cName VARCHAR(20) NOT NULL,
+		cID INT NOT NULL,
+		cName VARCHAR(20) NOT NULL,
     	cAddress VARCHAR(25) NOT NULL,
-	 bParcel INT NOT NULL,
+        cPhone INT(8),
+		bParcel INT NOT NULL,
+        cPass VARCHAR (15),
 primary key (cID),
 foreign key (bParcel) references buildings(bParcel));
+
+insert into buildings values ('
+insert into customers VALUES ('1111','TestPerson1','Nowherestreet 323','22334455','98756','1234');
