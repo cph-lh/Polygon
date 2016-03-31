f2dea02d928bc3d45d8a76aef46b67c22ba6457e
Jmach <Jmach@10.20.154.108>
2016-03-31 12:39:32 +0200

    building datamapper

--- src/java/domain/DM_Building.java
+++ src/java/domain/DM_Building.java
@@ -34,15 +34,36 @@ public class DM_Building {
 
         ResultSet res;
 
-        int cID;
-        String name;
-        String address;
-        String city;
-        int zip;
-        int parcel;
-        int size;
-        int floors;
-        int status;
+        String name = "";
+        String address = "";
+        String city = "";
+        int zip = 0;
+        int parcel = 0;
+        int size = 0;
+        int floors = 0;
+        int status = 0;
+
+        while (res.next())
+        {
+            try
+            {
+                name = res.getString("bName");
+                address = res.getString("bAddress");
+                city = res.getString("bCity");
+                zip = res.getInt("bZip");
+                parcel = res.getInt("bParcel");
+                size = res.getInt("bSize");
+                floors = res.getInt("bFloors");
+                status = res.getInt("bStatus");
+
+                Building building = new Building(cID, name, address, zip, city, parcel, size, floors, status);
+                buildingList.add(building);
+
+            } catch (SQLException ex)
+            {
+                Logger.getLogger(DM_Building.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
 
         return buildingList;
     }
