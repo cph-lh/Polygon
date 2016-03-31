974012f4e06941180bf2b99ddd30c430fa386e00
laurahartig <laurahartig@10.20.154.109>
2016-03-31 11:45:02 +0200

    OCD strikes again

--- src/java/domain/DM_Building.java
+++ src/java/domain/DM_Building.java
@@ -1,25 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package domain;
 
 import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author Jmach
- */
 public class DM_Building {
 
     Statement stmt;
 
     public String addBuilding(String name, String address, int parcel, int floors, int size, int status)
     {
-
         try
         {
             String query = "insert into buildings values('" + name + "','" + address + "','"
