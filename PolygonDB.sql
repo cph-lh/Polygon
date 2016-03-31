27a92ae25b8db5e4c86af477e6182768d750a5b7
Andreas <Andreas@10.20.156.109>
2016-03-31 13:29:33 +0200

    db changes to buildings

--- PolygonDB.sql
+++ PolygonDB.sql
@@ -22,9 +22,9 @@ CREATE TABLE buildings	(
         bZip INT (4) NOT NULL,
     	bParcel INT NOT NULL,        
     	bSize INT NOT NULL,
-        bFloors INT default null,
-    	bStatus INT default NULL,
+#        bFloors INT default null,
+#    	bStatus INT default NULL,
 foreign key (cID) references customers(cID));
 
-insert into customers VALUES ('1111','TestPerson1','22334455','gadegade 123','kbh','2222','1234');
+insert into customers VALUES ('1111','TestPerson1','22334455','gadegade 123','kbh','2222','1234')
 select * from customers; 
