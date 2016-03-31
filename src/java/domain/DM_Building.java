00e5c194294560fd0d046e7857ff6ad66f371f1b
laurahartig <laurahartig@10.20.154.109>
2016-03-31 13:06:42 +0200

    DM_Building edits (con, stmt, query)

--- src/java/domain/DM_Building.java
+++ src/java/domain/DM_Building.java
@@ -2,26 +2,22 @@ package domain;
 
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class DM_Building {
 
-    Statement stmt;
 
-    public String addBuilding(int id, String name, String address, int zip, String city, int parcel, int size)
+    public void addBuilding(int id, String name, String address, String city, int zip, int parcel, int size, int floors, int status)
     {
         try
         {
             String query = "insert into buildings values('" + id + "','" + name + "','"
-                    + address + "','" + zip + "','" + city + "','" + parcel + "','"+ size +"')";
-
-            stmt.executeUpdate(query);
-            return query;
+                    + address + "','" + city + "','" + zip + "','" + parcel + "','"+ size +"')";
+        
+            new Connector().connect().createStatement().executeUpdate(query);
+            
         } catch (SQLException ex)
         {
-            Logger.getLogger(DM_Building.class.getName()).log(Level.SEVERE, null, ex);
-            return null;
+            ex.printStackTrace();
         }
     }
 
