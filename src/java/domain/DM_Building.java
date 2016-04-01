de465d1cc5dd1f6ecee3db8a6ce394822288609f
Jmach <Jmach@10.20.154.108>
2016-04-01 09:19:13 +0200

    DM_building viewbuilding

--- src/java/domain/DM_Building.java
+++ src/java/domain/DM_Building.java
@@ -2,6 +2,8 @@ package domain;
 
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DM_Building {
 
@@ -24,43 +26,50 @@ public class DM_Building {
     public ArrayList<Building> buildings(int cID)
     {
 
-        ArrayList<Building> buildingList = new ArrayList();
-
-        String query = "select * from buildings where cID = '" + cID + "'";
-
-        ResultSet res;
-
-        String name = "";
-        String address = "";
-        String city = "";
-        int zip = 0;
-        int parcel = 0;
-        int size = 0;
-        int floors = 0;
-        int status = 0;
-
-/*        while (res.next())
+        try
         {
-            try
+            ArrayList<Building> buildingList = new ArrayList();
+            
+            String query = "select * from buildings where cID = '" + cID + "'";
+            
+            ResultSet res = new Connector().connect().createStatement().executeQuery(query);
+            
+            String name = "";
+            String address = "";
+            String city = "";
+            int zip = 0;
+            int parcel = 0;
+            int size = 0;
+            int floors = 0;
+            int status = 0;
+            
+                    while (res.next())
             {
-                name = res.getString("bName");
-                address = res.getString("bAddress");
-                city = res.getString("bCity");
-                zip = res.getInt("bZip");
-                parcel = res.getInt("bParcel");
-                size = res.getInt("bSize");
-                floors = res.getInt("bFloors");
-                status = res.getInt("bStatus");
-
-                Building building = new Building(cID, name, address, zip, city, parcel, size, floors, status);
-                buildingList.add(building);
-
+            try
+            {    
+            name = res.getString("bName");
+            address = res.getString("bAddress");
+            city = res.getString("bCity");
+            zip = res.getInt("bZip");
+            parcel = res.getInt("bParcel");
+            size = res.getInt("bSize");
+            floors = res.getInt("bFloors");
+            status = res.getInt("bStatus");
+            
+            Building building = new Building(cID, name, address, zip, city, parcel, size, floors, status);
+            buildingList.add(building);
+            
             } catch (SQLException ex)
             {
-                Logger.getLogger(DM_Building.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(DM_Building.class.getName()).log(Level.SEVERE, null, ex);
             }
+            }
+            
+            return null;
+        } catch (SQLException ex)
+        {
+            Logger.getLogger(DM_Building.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
         }
-*/
-        return null;
     }
 }
