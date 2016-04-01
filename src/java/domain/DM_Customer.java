5501615b138889aa38a7c75d9bc43501873309e7
Jmach <Jmach@10.20.154.108>
2016-04-01 10:22:34 +0200

    commit

--- src/java/domain/DM_Customer.java
+++ src/java/domain/DM_Customer.java
@@ -6,21 +6,18 @@ import java.util.logging.Logger;
 
 public class DM_Customer {
     
-    Statement stmt;
-
-    public String addCustomer(String name, String address, int phone, int ID, String password)
+    public void addCustomer(int id, String name, String address, int zip, String city, int phone, String password)
     {
         try
         {
-            String query = "insert into customers values('" + ID + "','" + name + "','"
-                    + phone + "','" + address + "','" + password + "')";
+            String query = "insert into buildings values('" + id + "','" + name + "','"
+                    + address + "','" + zip + "','" + city + "','" + phone +"')";
 
-            stmt.executeUpdate(query);
-            return query;
+            new Connector().connect().createStatement().executeUpdate(query);
+            
         } catch (SQLException ex)
         {
             Logger.getLogger(DM_Building.class.getName()).log(Level.SEVERE, null, ex);
-            return null;
         }
     }   
 }
