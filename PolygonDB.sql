83bb901ba5ca27e05f04d5cca822ed6c6fa5c976
laurahartig <laurahartig@10.20.156.40>
2016-04-05 11:23:27 +0200

    DB buildings edits

--- PolygonDB.sql
+++ PolygonDB.sql
@@ -1213,6 +1213,7 @@ INSERT INTO zipcodes VALUES ('9981', 'Jerup');
 INSERT INTO zipcodes VALUES ('9982', 'Ålbæk');
 INSERT INTO zipcodes VALUES ('9990', 'Skagen');
 insert into customers VALUES ('1111','TestPerson1','22334455','gadegade 123','2500','1234');
-select * from customers;
-select * from buildings;
+select *, (select city from zipcodes where zip = c.czip) as 'City' from customers c;
+insert into buildings VALUES (1111,'A1','Street 1234','2500',250,123,0,0);
+select *, (select city from zipcodes where zip = b.bzip) as 'City' from buildings b;
 
