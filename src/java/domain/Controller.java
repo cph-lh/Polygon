5a563e8abb3ee2969c146446354aec8aa1f22677
Jmach <Jmach@10.20.154.108>
2016-04-16 11:35:30 +0200

    viewFloorPlan

--- src/java/domain/Controller.java
+++ src/java/domain/Controller.java
@@ -1,54 +1,61 @@
 package domain;
+
 import data.Facade;
 import java.io.InputStream;
 import java.util.ArrayList;
 
-public class Controller
-{
+public class Controller {
+
     Facade f = new Facade();
-    
+
     public void addBuilding(int id, String name, String address, int zip, int parcel, int size, int year, int floors)
     {
         f.addBuilding(id, name, address, zip, parcel, size, year, floors);
     }
-    
+
     public void addCustomer(int id, String name, String address, int zip, int phone, String password)
     {
         f.addCustomer(id, name, address, zip, phone, password);
     }
-    
-    public ArrayList<Building> viewBuildings(int cID){
-       return f.viewBuildings(cID);
+
+    public ArrayList<Building> viewBuildings(int cID)
+    {
+        return f.viewBuildings(cID);
     }
-    
+
     public Customer getCustomer(int cID)
     {
         return f.getCustomer(cID);
     }
-    
+
     public void deleteBuilding(int bID)
     {
         f.deleteBuilding(bID);
     }
-    
+
     private void deleteAllBuildings(int cID)
     {
         f.deleteAllBuildings(cID);
     }
-    
+
     public void deleteCustomer(int cID)
     {
         deleteAllBuildings(cID);
         f.deleteCustomer(cID);
     }
-    
+
     public ArrayList<Customer> viewCustomers()
     {
         return f.viewCustomers();
     }
-    
+
     public void addFloorPlan(int bID, InputStream input)
     {
         f.addFloorPlan(bID, input);
-    }  
+    }
+
+    public byte[] viewFloorPlan(int bID)
+    {
+        return f.viewFloorPlan(bID);
+    }
 }
