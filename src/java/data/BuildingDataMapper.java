344a749357b3168736435ab64ccde61639552319
Jmach <Jmach@10.20.154.108>
2016-04-16 11:24:20 +0200

    getFloorPlan

--- src/java/data/BuildingDataMapper.java
+++ src/java/data/BuildingDataMapper.java
@@ -15,7 +15,7 @@ public class BuildingDataMapper {
         {
             String query = "insert into buildings(cID,bName,bAddress,zip,bParcel,bSize,bYear,bFloors)"
                     + " values('" + id + "','" + name + "','"
-                    + address + "','" + zip + "','" + parcel + "','" + size + "',"+floors+","+year+");";
+                    + address + "','" + zip + "','" + parcel + "','" + size + "'," + floors + "," + year + ");";
             new Connector().connect().createStatement().executeUpdate(query);
 
         } catch (SQLException ex)
@@ -23,27 +23,29 @@ public class BuildingDataMapper {
             Logger.getLogger(BuildingDataMapper.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    public void deleteBuilding(int bID){
-         try
+
+    public void deleteBuilding(int bID)
+    {
+        try
         {
             String query = "delete from buildings where bID = '" + bID + "';";
 
             new Connector().connect().createStatement().executeUpdate(query);
-            
+
         } catch (SQLException ex)
         {
             Logger.getLogger(BuildingDataMapper.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    public void deleteAllBuildings(int cID){
-         try
+
+    public void deleteAllBuildings(int cID)
+    {
+        try
         {
             String query = "delete from buildings where cID = '" + cID + "';";
 
             new Connector().connect().createStatement().executeUpdate(query);
-            
+
         } catch (SQLException ex)
         {
             Logger.getLogger(BuildingDataMapper.class.getName()).log(Level.SEVERE, null, ex);
@@ -56,15 +58,15 @@ public class BuildingDataMapper {
         {
             ArrayList<Building> buildingList = new ArrayList();
 
-            String query = "select * from zipcodes natural join buildings where cID = '"+ cID +"'";
+            String query = "select * from zipcodes natural join buildings where cID = '" + cID + "'";
 
             ResultSet res = new Connector().connect().createStatement().executeQuery(query);
 
             while (res.next())
-            {               
+            {
                 int bID = res.getInt("bID");
                 String name = res.getString("bName");
-                String address = res.getString("bAddress");               
+                String address = res.getString("bAddress");
                 int parcel = res.getInt("bParcel");
                 int size = res.getInt("bSize");
                 int floors = res.getInt("bFloors");
@@ -80,22 +82,48 @@ public class BuildingDataMapper {
         } catch (SQLException ex)
         {
             Logger.getLogger(BuildingDataMapper.class.getName()).log(Level.SEVERE, null, ex);
-            
+
             return null;
         }
     }
-    
-        public void addFloorPlan(int bID, InputStream input)
+
+    public void addFloorPlan(int bID, InputStream input)
     {
         try
         {
             PreparedStatement state = new Connector().connect().prepareStatement("insert into floor_plan values (?,?)");
             state.setInt(1, bID);
             state.setBlob(2, input);
-            state.executeUpdate();           
+            state.executeUpdate();
         } catch (SQLException ex)
         {
             Logger.getLogger(PictureDataMapper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public byte[] getFloorPlan(int bID)
+    {
+
+        try
+        {
+            Blob img;
+            byte[] imgData = null;
+
+            String query = "select floor_plan from floor_plan where bID =" + bID;
+
+            ResultSet res = new Connector().connect().createStatement().executeQuery(query);
+
+            while (res.next())
+            {
+                img = res.getBlob(2);
+                imgData = img.getBytes(1, (int) img.length());
+            }
+
+            return imgData;
+        } catch (SQLException ex)
+        {
+            Logger.getLogger(BuildingDataMapper.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
         }
     }
 }
