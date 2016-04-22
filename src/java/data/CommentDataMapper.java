f05b95bc0bf57de9d88b95a54824ae2a2518c268
Jmach <Jmach@192.168.0.12>
2016-04-22 12:35:36 +0200

    RESCUE!!!! :D

--- src/java/data/CommentDataMapper.java
+++ src/java/data/CommentDataMapper.java
@@ -1,17 +1,51 @@
 package data;
 
-import java.sql.SQLException;
+import domain.Comment;
+import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class CommentDataMapper {
-
-    public void addComment(int bID, String column, String comment)
-    {
-        String query = "update comments set '" + column + "'='" + comment + "' where bID ="+bID+";";
+    public void createComment(int bID, Comment comment){
         try
         {
-            new Connector().connect().createStatement().executeUpdate(query);
+            String sql = "INSERT INTO `polygondb`.`comments` (`bID`,`bOutsideC`,`roofC`,`outerWallsC`,`wallsC`,`celingC`,`floorC`,`windows_doorsC`)VALUES(?,?,?,?,?,?,?,?)";
+            PreparedStatement pstmt = new Connector().connect().prepareStatement(sql);
+            pstmt.setInt(1, bID);
+            pstmt.setString(2, comment.getbOutsideC());
+            pstmt.setString(3, comment.getRoofC());
+            pstmt.setString(4, comment.getOuterWallsC());
+            pstmt.setString(5, comment.getWallsC());
+            pstmt.setString(6, comment.getCelingC());
+            pstmt.setString(7, comment.getFloorC());
+            pstmt.setString(8, comment.getWindows_doorsC());
+            
+            
+        } catch (SQLException ex)
+        {
+            Logger.getLogger(CommentDataMapper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    public void updateCommentColumn(int bID, String column, String comment)
+    {
+        try
+        {
+            int checkBID = -1;
+
+            String query = "select bID from comments where bID =" + bID + ";";
+            ResultSet res = new Connector().connect().createStatement().executeQuery(query);
+
+            if (res.next())
+            {
+                checkBID = res.getInt("bID");
+                System.out.println("------checkbdid--------"+checkBID);
+            }
+            if (checkBID != -1)
+            {
+                String updateQuery = "update comments set " + column + "='" + comment + "' where bID =" + bID + ";";
+                new Connector().connect().createStatement().executeUpdate(updateQuery);
+            } 
         } catch (SQLException ex)
         {
             Logger.getLogger(CommentDataMapper.class.getName()).log(Level.SEVERE, null, ex);
--- src/java/domain/Comment.java
+++ src/java/domain/Comment.java
@@ -2,5 +2,99 @@ package domain;
 
 public class Comment
 {
+    String bID, bOutsideC, roofC, outerWallsC, wallsC, celingC ,floorC ,windows_doorsC;
+
+    public Comment(String bID, String bOutsideC, String roofC, String outerWallsC, String wallsC, String celingC, String floorC, String windows_doorsC)
+    {
+        this.bID = bID;
+        this.bOutsideC = bOutsideC;
+        this.roofC = roofC;
+        this.outerWallsC = outerWallsC;
+        this.wallsC = wallsC;
+        this.celingC = celingC;
+        this.floorC = floorC;
+        this.windows_doorsC = windows_doorsC;
+    }
+    
+    
+    public String getbID()
+    {
+        return bID;
+    }
+
+    public void setbID(String bID)
+    {
+        this.bID = bID;
+    }
+
+    public String getbOutsideC()
+    {
+        return bOutsideC;
+    }
+
+    public void setbOutsideC(String bOutsideC)
+    {
+        this.bOutsideC = bOutsideC;
+    }
+
+    public String getRoofC()
+    {
+        return roofC;
+    }
+
+    public void setRoofC(String roofC)
+    {
+        this.roofC = roofC;
+    }
+
+    public String getOuterWallsC()
+    {
+        return outerWallsC;
+    }
+
+    public void setOuterWallsC(String outerWallsC)
+    {
+        this.outerWallsC = outerWallsC;
+    }
+
+    public String getWallsC()
+    {
+        return wallsC;
+    }
+
+    public void setWallsC(String wallsC)
+    {
+        this.wallsC = wallsC;
+    }
+
+    public String getCelingC()
+    {
+        return celingC;
+    }
+
+    public void setCelingC(String celingC)
+    {
+        this.celingC = celingC;
+    }
+
+    public String getFloorC()
+    {
+        return floorC;
+    }
+
+    public void setFloorC(String floorC)
+    {
+        this.floorC = floorC;
+    }
+
+    public String getWindows_doorsC()
+    {
+        return windows_doorsC;
+    }
+
+    public void setWindows_doorsC(String windows_doorsC)
+    {
+        this.windows_doorsC = windows_doorsC;
+    }
     
 }
